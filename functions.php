2e962cfff9bf47e5cca047001e53653e4796e933
jexidev <169895029+jexidev@users.noreply.github.com>
2026-02-19 13:23:08 +0000

    Added theme supports to function.php

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
 
+// Load up CSS with versioning
 add_action('wp_enqueue_scripts', 'jexidev_enqueue_assets');
 function jexidev_enqueue_assets() {
     
@@ -13,5 +14,19 @@ function jexidev_enqueue_assets() {
         $main_css_ver
     );
 
+}
+
+// Register theme supports, menus, and other initial setup
+add_action('after_setup_theme', 'jexidev_theme_setup');
+function jexidev_theme_setup() {
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_theme_support('html5', array('search-form', 'comment-form', 'comment-list', 'gallery', 'caption'));
+    add_theme_support('responsive-embeds');
+    
+    register_nav_menus(array(
+        'primary' => 'Primary Menu',
+        'footer' => 'Footer Menu'
+    ));
 }
 
