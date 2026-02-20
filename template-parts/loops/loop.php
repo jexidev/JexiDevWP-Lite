f2d09544bb4adb2b11924657e3b0002bc7a9d36f
jexidev <169895029+jexidev@users.noreply.github.com>
2026-02-20 10:20:23 +0000

    Initial commit for loop template part.

---
+++ template-parts/loops/loop.php
@@ -0,0 +1,19 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class('diary-entry-container'); ?>>
+
+    <header class="entry-header">
+        <h1 class="entry-title"><?php the_title(); ?></h1>
+    </header>
+
+    <div class="entry-content">
+        <?php 
+        if ( has_post_thumbnail() ) { 
+            the_post_thumbnail( 'full' ); 
+        } 
+        ?>
+
+        <?php the_content(); ?>
+
+        <?php wp_link_pages(); ?>
+    </div>
+
+</article>
