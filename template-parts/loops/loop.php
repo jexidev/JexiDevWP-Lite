e920bb67538431159effba1c1f487bed109ba81f
jexidev <169895029+jexidev@users.noreply.github.com>
2026-02-20 11:36:36 +0000

    Stripped out redundent class left over from jexidev.site

--- template-parts/loops/loop.php
+++ template-parts/loops/loop.php
@@ -1,4 +1,4 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class('diary-entry-container'); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
     <header class="entry-header">
         <h1 class="entry-title"><?php the_title(); ?></h1>
