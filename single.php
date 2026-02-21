0f0c608077928c62c6bf656456eead4403513ac9
jexidev <169895029+jexidev@users.noreply.github.com>
2026-02-21 12:16:39 +0000

    Added spacing for parenthesis

--- single.php
+++ single.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 
 <main id="site-content" role="main">
-    <?php if (have_posts()) {
-        while (have_posts()) {
+    <?php if ( have_posts() ) {
+        while ( have_posts() ) {
             the_post();
             get_template_part('template-parts/loops/loop', 'single');
         }
