6a43740c081accc50e3a34e1b0897217a6c88509
jexidev <169895029+jexidev@users.noreply.github.com>
2026-02-21 12:39:09 +0000

    Converted { if loop syntax for : to match the rest of .php files.

--- single.php
+++ single.php
@@ -1,14 +1,14 @@
 <?php get_header(); ?>
 
 <main id="site-content" role="main">
-    <?php if ( have_posts() ) {
-        while ( have_posts() ) {
-            the_post();
-            get_template_part('template-parts/loops/loop', 'single');
-        }
-    }
-    ?>
-
+    <?php if ( have_posts() ) : ?>
+        <?php while ( have_posts() ) : the_post(); ?>
+            <?php get_template_part('template-parts/loops/loop', 'single'); ?>
+        <?php endwhile; ?>
+    <?php else : ?>
+        <p>No content found.</p>
+    <?php endif; ?>
 </main>
+
 
 <?php get_footer(); ?>
