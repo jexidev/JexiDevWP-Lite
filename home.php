f0e258a7f0f97817cc391ddcd9e23fd6c462857a
jexidev <169895029+jexidev@users.noreply.github.com>
2026-02-21 11:58:34 +0000

    Changed bracket sapcing around have_posts

--- home.php
+++ home.php
@@ -2,7 +2,7 @@
 
 <main id="site-content" role="main">
 
-    <?php if ( have_posts() ) : ?>
+    <?php if (have_posts()) : ?>
         <div class="posts">
             <?php while ( have_posts() ) : the_post(); ?>
                 <?php get_template_part('template-parts/loops/loop', 'post'); ?>
