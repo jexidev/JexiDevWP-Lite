6caa863278e1d231b8736ec7795e8731bb4e83f0
jexidev <169895029+jexidev@users.noreply.github.com>
2026-02-21 13:37:12 +0000

    Wrapped loop in main tag to follow other .php page conventions.

--- page.php
+++ page.php
@@ -1,11 +1,13 @@
 <?php get_header(); ?>
 
-<?php 
-if ( have_posts() ) : 
-    while ( have_posts() ) : the_post();
-        get_template_part('template-parts/loops/loop', 'page');
-    endwhile;
-endif;
-?>
+<main id="site-content" role="main">
+    <?php 
+    if ( have_posts() ) : 
+        while ( have_posts() ) : the_post();
+            get_template_part('template-parts/loops/loop', 'page');
+        endwhile;
+    endif;
+    ?>
+</main>
 
 <?php get_footer(); ?>
