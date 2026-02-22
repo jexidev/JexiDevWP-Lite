fc567b4fc13319aaa5ce240d08dfbb44aa033d29
jexidev <169895029+jexidev@users.noreply.github.com>
2026-02-22 12:15:55 +0000

    Transformed function.php into a loader pulling from inc/functions

--- functions.php
+++ functions.php
@@ -1,49 +1,5 @@
 <?php
 
-// Load up CSS, JS, and other assets
-add_action('wp_enqueue_scripts', 'jexidevwp_enqueue_assets');
-function jexidevwp_enqueue_assets() {
-    
-    // CSS
-    $main_css_path = get_stylesheet_directory() . '/style.css';
-    $main_css_ver = file_exists($main_css_path) ? filemtime($main_css_path) : false;
-
-    wp_enqueue_style(
-        'jexidevwp-style',
-        get_stylesheet_uri(),
-        array(),
-        $main_css_ver
-    );
-
-    // JS
-    $main_js_path = get_stylesheet_directory() . '/js/custom-scripts.js';
-    $main_js_ver = file_exists($main_js_path) ? filemtime($main_js_path) : false;
-
-    wp_enqueue_script(
-        'jexidevwp-scripts',
-        get_stylesheet_directory_uri() . '/js/custom-scripts.js',
-        array(),
-        $main_js_ver,
-        true
-    );
-}
-
-// Register theme supports, menus, and other initial setup
-add_action('after_setup_theme', 'jexidevwp_theme_setup');
-function jexidevwp_theme_setup() {
-    add_theme_support('title-tag');
-    add_theme_support('post-thumbnails');
-    add_theme_support('html5', array('search-form', 'comment-form', 'comment-list', 'gallery', 'caption'));
-    add_theme_support('responsive-embeds');
-    
-    register_nav_menus(array(
-        'primary' => 'Primary Menu',
-        'mobile' => 'Mobile',
-        'footer' => 'Footer Menu'
-    ));
-}
-
-// Footer attribution toggle
-function jexidevwp_show_attribution() {
-    return apply_filters('jexidevwp_show_attribution', true);
-}
+require get_theme_file_path( 'inc/functions/enqueue.php' );
+require get_theme_file_path( 'inc/functions/setup.php' );
+require get_theme_file_path( 'inc/functions/toggle.php' );
