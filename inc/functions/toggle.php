90ecf23175e4eb8bd3e38205b33ae0901eeeb15e
jexidev <169895029+jexidev@users.noreply.github.com>
2026-02-22 14:06:04 +0000

    Added remove WP version toggle.

--- inc/functions/toggle.php
+++ inc/functions/toggle.php
@@ -1,16 +1,22 @@
 <?php
-
+// -1- GENERAL SITE TOGGLES
 // Footer attribution toggle
 function jexidevwp_show_attribution() {
     return apply_filters( 'jexidevwp_show_attribution', true );
 }
 
-// SECURITY TOGGLES
+// -2- SECURITY TOGGLES
 // XML-RPC default true
 function jexidevwp_disable_xmlrpc() {
     return apply_filters( 'jexidevwp_disable_xmlrpc', true );
 }
 
+// Remove WP Version
+function jexidevwp_remove_wp_version() {
+    return apply_filters('jexidevwp_remove_wp_version', true);
+}
+
+// -3- CLEANUP
 // Disable emoji 
 function jexidevwp_disable_emojis() {
     return apply_filters( 'jexidevwp_disable_emojis', true );
