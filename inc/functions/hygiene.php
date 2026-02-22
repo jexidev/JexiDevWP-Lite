2a3f56899e44f3af31709f64f9f78dab642e715b
jexidev <169895029+jexidev@users.noreply.github.com>
2026-02-22 13:37:56 +0000

    Put comments in place

--- inc/functions/hygiene.php
+++ inc/functions/hygiene.php
@@ -2,11 +2,14 @@
 
 // Collection of security functions - can all be toggled in toggle.php
 
+// -- SECURITY
 // Disable xml-rpc - reduces DDOS
 if ( jexidevwp_disable_xmlrpc() ) {
     add_filter( 'xmlrpc_enabled', '__return_false' );
 }
 
+
+// -- CLEANUP
 // Disable legacy emoji script
 if ( jexidevwp_disable_emojis() ) {
     remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
