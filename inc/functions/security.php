90d3160f743f080c6890f813564ee9f3ec7bbfc8
jexidev <169895029+jexidev@users.noreply.github.com>
2026-02-22 13:02:38 +0000

    Added disable emoji rules to security.php

--- inc/functions/security.php
+++ inc/functions/security.php
@@ -1,7 +1,14 @@
 <?php
 
+// Collection of security functions - can all be toggled in toggle.php
+
 // Disable xml-rpc - reduces DDOS
 if ( jexidevwp_disable_xmlrpc() ) {
     add_filter( 'xmlrpc_enabled', '__return_false' );
 }
 
+// Disable legacy emoji script
+if ( jexidevwp_disable_emojis() ) {
+    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+    remove_action( 'wp_print_styles', 'print_emoji_styles' );
+}
