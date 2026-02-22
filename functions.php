0ca3505df151f72c5ddcec25b86911d4c10430bf
jexidev <169895029+jexidev@users.noreply.github.com>
2026-02-22 13:30:24 +0000

    Changed security.php to hygiene.php

--- functions.php
+++ functions.php
@@ -3,4 +3,4 @@
 require_once get_theme_file_path( 'inc/functions/enqueue.php' );
 require_once get_theme_file_path( 'inc/functions/setup.php' );
 require_once get_theme_file_path( 'inc/functions/toggle.php' );
-require_once get_theme_file_path( 'inc/functions/security.php');
+require_once get_theme_file_path( 'inc/functions/hygiene.php');
