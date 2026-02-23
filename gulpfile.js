a0f4b5d4fb984a845d942c81640288d24728d244
jexidev <169895029+jexidev@users.noreply.github.com>
2026-02-23 10:33:58 +0000

    Added compilation rules to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,5 +6,23 @@ const cssnano = require('cssnano');
 
 // Paths
 const paths = {
-    scss = 
+    scss: './assets/scss/**/*.scss',
+    css: './assets/css'
+};
+
+// Compile SCSS
+function styles() {
+    return gulp.src(paths.scss)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(gulp.dest(paths.css));
 }
+
+// Watch task
+function watchFiles() {
+    gulp.watch(paths.scss, styles);
+}
+
+exports.styles = styles;
+exports.watch = watchFiles;
+exports.defaults = gulp.series(styles, watchFiles);
