a37e6f61d5fc73b53fd52d760b4fdb2868b9f83c
jexidev <169895029+jexidev@users.noreply.github.com>
2026-02-23 10:11:48 +0000

    Added prefix slashes to folder paths.

--- inc/functions/enqueue.php
+++ inc/functions/enqueue.php
@@ -5,23 +5,24 @@ add_action('wp_enqueue_scripts', 'jexidevwp_enqueue_assets');
 function jexidevwp_enqueue_assets() {
     
     // CSS
-    $main_css_path = get_stylesheet_directory() . 'assets/css/style.css';
+    $main_css_path = get_stylesheet_directory() . '/assets/css/style.css';
     $main_css_ver = file_exists($main_css_path) ? filemtime($main_css_path) : false;
 
     wp_enqueue_style(
         'jexidevwp-style',
-        get_stylesheet_uri(),
+        get_stylesheet_directory_uri() . '/assets/css/style.css',
         array(),
         $main_css_ver
     );
 
+
     // JS
-    $main_js_path = get_stylesheet_directory() . 'assets/js/custom-scripts.js';
+    $main_js_path = get_stylesheet_directory() . '/assets/js/custom-scripts.js';
     $main_js_ver = file_exists($main_js_path) ? filemtime($main_js_path) : false;
 
     wp_enqueue_script(
         'jexidevwp-scripts',
-        get_stylesheet_directory_uri() . 'assets/js/custom-scripts.js',
+        get_stylesheet_directory_uri() . '/assets/js/custom-scripts.js',
         array(),
         $main_js_ver,
         true
