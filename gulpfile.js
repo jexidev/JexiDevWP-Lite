9f8e26c674f01ce613701fcf5a414b0706f53662
jexidev <169895029+jexidev@users.noreply.github.com>
2026-02-23 10:06:04 +0000

    Moved JS, SCSS, and CSS into assets/<the folder>

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+// Paths
+const paths = {
+    scss = 
+}
