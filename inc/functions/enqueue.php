dd382f7ef71d070e7ec64b54167a05ddadd11ca8
jexidev <169895029+jexidev@users.noreply.github.com>
2026-02-23 10:08:11 +0000

    Updated paths for enqueues

--- inc/functions/enqueue.php
+++ inc/functions/enqueue.php
@@ -5,7 +5,7 @@ add_action('wp_enqueue_scripts', 'jexidevwp_enqueue_assets');
 function jexidevwp_enqueue_assets() {
     
     // CSS
-    $main_css_path = get_stylesheet_directory() . '/style.css';
+    $main_css_path = get_stylesheet_directory() . 'assets/css/style.css';
     $main_css_ver = file_exists($main_css_path) ? filemtime($main_css_path) : false;
 
     wp_enqueue_style(
@@ -16,12 +16,12 @@ function jexidevwp_enqueue_assets() {
     );
 
     // JS
-    $main_js_path = get_stylesheet_directory() . '/js/custom-scripts.js';
+    $main_js_path = get_stylesheet_directory() . 'assets/js/custom-scripts.js';
     $main_js_ver = file_exists($main_js_path) ? filemtime($main_js_path) : false;
 
     wp_enqueue_script(
         'jexidevwp-scripts',
-        get_stylesheet_directory_uri() . '/js/custom-scripts.js',
+        get_stylesheet_directory_uri() . 'assets/js/custom-scripts.js',
         array(),
         $main_js_ver,
         true
