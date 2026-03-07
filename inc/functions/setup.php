9700da3df10e28a52018b28d23799795aaecceea
jexidev <169895029+jexidev@users.noreply.github.com>
2026-03-07 12:21:04 +0000

    Added custom_logo image handling and image sizes.

--- inc/functions/setup.php
+++ inc/functions/setup.php
@@ -7,6 +7,13 @@ function jexidevwp_theme_setup() {
     add_theme_support('post-thumbnails');
     add_theme_support('html5', array('search-form', 'comment-form', 'comment-list', 'gallery', 'caption'));
     add_theme_support('responsive-embeds');
+    
+    add_theme_support('custom-logo', array(
+    'height'      => 300,
+    'width'       => 300,
+    'flex-height' => true,
+    'flex-width'  => true,
+));
 
     // Custom image sizes (JDWP Lite v1.1.0 feature)
     add_image_size('jexidevwp-logo', 300, 0, false);
