e5ab2163f46ec082e9ea51c3451155b881c7b0d2
jexidev <169895029+jexidev@users.noreply.github.com>
2026-03-07 11:55:56 +0000

    Added default img support to theme

--- inc/functions/setup.php
+++ inc/functions/setup.php
@@ -7,7 +7,13 @@ function jexidevwp_theme_setup() {
     add_theme_support('post-thumbnails');
     add_theme_support('html5', array('search-form', 'comment-form', 'comment-list', 'gallery', 'caption'));
     add_theme_support('responsive-embeds');
-    
+
+    // Custom image sizes (JDWP Lite v1.1.0 feature)
+    add_image_size('jexidevwp-logo', 300, 0, false);
+    add_image_size('jexidevwp-thumb', 600, 400, true);   
+    add_image_size('jexidevwp-medium', 1200, 800, true);  
+    add_image_size('jexidevwp-large', 2000, 1200, true);  
+
     register_nav_menus(array(
         'primary' => 'Primary Menu',
         'mobile' => 'Mobile',
